240013f59cd336429367cd890e9054d8484a125c
Adam Hanusek <hanusek.adam@gmail.com>
2022-10-09 13:23:49 +0200

    First commit

---
+++ Ukol č.7/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+int m;
+int n;
+
+WriteLine("Zadej n");
+n = Convert.ToInt32(ReadLine());
+
+m = Convert.ToInt32(ReadLine());
+
+int startNumber = 1;
+int tempNumber = startNumber + n; 
+
+Write(startNumber + " ");
+for(int i = 0; i < n; i++){
+    
+}
