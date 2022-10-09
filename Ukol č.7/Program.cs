8122687d394fb2e4fcf0ede03b3206ee38d4c9ba
Adam Hanusek <hanusek.adam@gmail.com>
2022-10-09 14:24:11 +0200

    All done and working

--- Ukol č.7/Program.cs
+++ Ukol č.7/Program.cs
@@ -1,17 +1,35 @@
 ﻿using static System.Console;
 
-int m;
-int n;
-
-WriteLine("Zadej n");
-n = Convert.ToInt32(ReadLine());
-
-m = Convert.ToInt32(ReadLine());
-
-int startNumber = 1;
-int tempNumber = startNumber + n; 
-
-Write(startNumber + " ");
-for(int i = 0; i < n; i++){
-    
+namespace consoleApp{
+    class cv7 {
+      static void Main() {
+        int m;
+        int n;
+        
+        WriteLine("Zadej n");
+        n = int.Parse(ReadLine());
+        WriteLine("Zadej m");
+        m = int.Parse(ReadLine());
+        
+        vypisMatici(m, n);
+      }
+      
+      static void vypisMatici(int m, int n) {
+          int startNumber = 1;
+            int number;
+            
+            
+            for(int i = 0; i < n; i++){
+                number = startNumber;
+                for(int j = 0; j < m; j++){
+                    Write(number);
+                    Write(" ");
+                    number += n;
+                }
+                Write("\n");
+                startNumber += 1;
+            }
+      }
+      
+    }
 }
