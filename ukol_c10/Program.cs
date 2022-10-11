39747edf493cb5e73cf80723ff876b7536a8c479
Adam Hanusek <hanusek.adam@gmail.com>
2022-10-11 13:00:51 +0200

    9 and 10 done

---
+++ ukol_c10/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace consoleApp {
+    class cv9 {
+        static void Main() {
+            WriteLine("Zadej retezec");
+            string input = ReadLine();
+            char[] splittedInput = input.ToCharArray();
+            int len = input.Length;
+            vypisPozpatku(splittedInput, len);
+            
+        }
+
+        static void vypisPozpatku(char[] input, int len) {
+            for(int i = len - 1; i >= 0; i--)
+            {
+                Write(input[i]);
+            }
+        }
+    }
+}
---
+++ ukol_c9/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace consoleApp {
+    class cv9 {
+        static void Main() {
+            WriteLine("Zadej retezec");
+            string input = ReadLine();
+            char[] splittedInput = input.ToCharArray();
+            vypisVertikalne(splittedInput);
+            
+        }
+
+        static void vypisVertikalne(char[] input) {
+            foreach (char letter in input)
+            {
+                WriteLine(letter);
+            }
+        }
+    }
+}
